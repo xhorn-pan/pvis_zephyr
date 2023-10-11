3e51ccdb9e70e62727a8ee3ae1e3e824061f7887
Xinghua Pan <23328176+xhorn-pan@users.noreply.github.com>
2023-10-11 13:00:45 -0400

    on board led working

--- app/src/main.c
+++ app/src/main.c
@@ -3,46 +3,67 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
-#include <app_version.h>
+#include <errno.h>
+#include <string.h>
 
+#define LOG_LEVEL 4
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+LOG_MODULE_REGISTER(main, LOG_LEVEL);
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/led_strip.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/sys/util.h>
+
+#define STRIP_NODE DT_ALIAS(led_strip)
+#define STRIP_NUM_PIXELS  DT_PROP(DT_ALIAS(led_strip), chain_length)
+
+#define DELAY_TIME K_MSEC(50)
+#define RGB(_r, _g, _b) {.r=(_r), .g=(_g), .b=(_b)}
+
+static const struct led_rgb colors[] = {
+  RGB(0x0f, 0x00, 0x00),
+  RGB(0x00, 0x0f, 0x00),
+  RGB(0x00, 0x00, 0x0f),
+};
+
+struct led_rgb pixels[STRIP_NUM_PIXELS];
+static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
 
 int main(void)
 {
-	int ret;
-	const struct device *sensor;
+  size_t cursor = 0, color = 0;
+  int rc;
 
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
+  if(device_is_ready(strip)) {
+    LOG_INF("Found LED strip device %s", strip->name);
+  } else {
+    LOG_ERR("LED strip device %s is not ready", strip->name);
+    return 0;
+  }
 
-	sensor = DEVICE_DT_GET(DT_NODELABEL(examplesensor0));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return 0;
-	}
+  LOG_INF("Displaying pattern on strip");
+  while(1) {
+    memset(&pixels, 0x00, sizeof(pixels));
+    memcpy(&pixels[cursor], &colors[color], sizeof(struct led_rgb));
+    rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
 
-	while (1) {
-		struct sensor_value val;
+    if (rc) {
+      LOG_ERR("couldn't update strip: %d", rc);
+    }
 
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return 0;
-		}
+    cursor++;
+    if (cursor >= STRIP_NUM_PIXELS) {
+      cursor = 0;
+      color++;
+      if(color == ARRAY_SIZE(colors)) {
+	color = 0;
+      }
+    }
 
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return 0;
-		}
-
-		printk("Sensor value: %d\n", val.val1);
-
-		k_sleep(K_MSEC(1000));
-	}
-
-	return 0;
+    k_sleep(DELAY_TIME);
+  }
+  return 0;
 }
 
